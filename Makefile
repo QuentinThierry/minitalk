0347add4ae2a257168075716c468e91ba427d573
Quentin THIERRY <qthierry@e3r10p3.clusters.42paris.fr>
2023-01-04 16:41:35 +0100

    [ADD] rules ans srcs for bonuses

--- Makefile
+++ Makefile
@@ -1,13 +1,19 @@
 SRCSSERVEUR = Srcs/serveur.c Srcs/utils.c
 OBJSSERVEUR = $(SRCSSERVEUR:.c=.o)
-
 SRCSCLIENT = Srcs/client.c Srcs/utils.c
 OBJSCLIENT = $(SRCSCLIENT:.c=.o)
+
+SRCSSERVEUR_BONUS = Srcs/serveur_bonus.c Srcs/utils_bonus.c
+OBJSSERVEUR_BONUS = $(SRCSSERVEUR_BONUS:.c=.o)
+SRCSCLIENT_BONUS = Srcs/client_bonus.c Srcs/utils_bonus.c
+OBJSCLIENT_BONUS = $(SRCSCLIENT_BONUS:.c=.o)
 
 DEPS = Includes/serveur.h Includes/client.h 
 
 NAME = server
 NAME1 = client
+NAME_BONUS = server_bonus
+NAME1_BONUS = client_bonus
 
 all: $(NAME) $(NAME1)
 
@@ -17,16 +23,25 @@ $(FLAGS):
 	-Wextra -Wall -Werror
 
 $(NAME): $(OBJSSERVEUR)
-	$(CC) $(FLAGS) $(OBJSSERVEUR) -o $(NAME)
+	$(CC) $(FLAGS) $(SRCSSERVEUR) -o $(NAME)
 
 $(NAME1): $(OBJSCLIENT)
 	$(CC) $(FLAGS) $(SRCSCLIENT) -o $(NAME1)
 
+$(NAME_BONUS): $(OBJSSERVEUR_BONUS)
+	$(CC) $(FLAGS) $(SRCSSERVEUR_BONUS) -o $(NAME_BONUS)
+
+$(NAME1_BONUS): $(OBJSCLIENT_BONUS)
+	$(CC) $(FLAGS) $(SRCSCLIENT_BONUS) -o $(NAME1_BONUS)
+
+bonus: $(NAME_BONUS) $(NAME1_BONUS)
+
 clean:
 	rm -f $(OBJSCLIENT) $(OBJSSERVEUR)
+	rm -f $(OBJSCLIENT_BONUS) $(OBJSSERVEUR_BONUS)
 
 fclean: clean
-	rm -f server client
+	rm -f $(NAME) $(NAME1) $(NAME_BONUS) $(NAME1_BONUS)
 
 re: fclean all
 
---
+++ Srcs/client_bonus.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qthierry <qthierry@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/05 10:31:15 by qthierry          #+#    #+#             */
+/*   Updated: 2022/12/09 14:27:54 by qthierry         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../Includes/client.h"
+
+int	g_has_return_sig;
+
+void	set_ready(int to_add, siginfo_t *info, void *vp)
+{
+	(void)to_add;
+	(void)info;
+	(void)vp;
+	g_has_return_sig = 1;
+}
+
+void	send_length(int size, pid_t pid)
+{
+	size_t	j;
+	size_t	size_int;
+
+	j = 0;
+	size_int = sizeof(int) * 8;
+	while (j < size_int - 1)
+	{
+		g_has_return_sig = 0;
+		if (j == 0)
+			size <<= 1;
+		if (!(size & 0x80000000))
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		size <<= 1;
+		j++;
+		while (!g_has_return_sig)
+			;
+	}
+}
+
+void	send_string(size_t size, char const **argv, pid_t pid)
+{
+	size_t	i;
+	size_t	j;
+	size_t	value;
+	size_t	size_char;
+
+	i = 0;
+	value = size;
+	size_char = 8 * sizeof(char);
+	while (i < size)
+	{
+		j = 0;
+		value = argv[2][i];
+		while (j++ < size_char)
+		{
+			g_has_return_sig = 0;
+			if (!(value & 0x80))
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			value <<= 1;
+			while (!g_has_return_sig)
+				;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char const **argv)
+{
+	int					pid;
+	size_t				size;
+	struct sigaction	act;
+
+	g_has_return_sig = 0;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = SA_SIGINFO;
+	act.sa_sigaction = &set_ready;
+	sigaction(SIGUSR1, &act, NULL);
+	if (argc != 3)
+		return (EXIT_FAILURE);
+	pid = ft_atoi(argv[1]);
+	size = ft_strlen(argv[2]) + 1;
+	send_length(size, pid);
+	send_string(size, argv, pid);
+	return (EXIT_SUCCESS);
+}
